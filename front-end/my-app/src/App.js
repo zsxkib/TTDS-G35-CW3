5a2609af6af3c37f2c1aa7a07cda75cca6d81b6a
winilau <winilau22@gmail.com>
2022-01-26 03:14:18 +0000

    front end

--- front-end/my-app/src/App.js
+++ front-end/my-app/src/App.js
@@ -1,8 +1,20 @@
-import Search from './search';
+import './App.css';
+import { Grid } from '@material-ui/core';
+import logo from './logo.png'
+import SearchBar from './search';
 
 const App = () => {
     return (
-        <Search />
+      <Grid 
+        container 
+        justify="center" 
+        alignItems="center" 
+        direction="column"
+        style={{minHeight:"100vh"}}
+      >
+          <img src={logo} alt="Logo" className="logo"/>
+        <SearchBar/>
+      </Grid>
     );
 }
 
