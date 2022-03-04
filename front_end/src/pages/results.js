9016ab10680678903db2f7ecc5b02a64219b24a5
Sakib Ahamed <raptor5600@gmail.com>
2022-03-04 17:58:11 +0000

    basic front end now works entirely

--- front_end/src/pages/results.js
+++ front_end/src/pages/results.js
@@ -1,67 +1,44 @@
-import { IconButton, TextField} from '@material-ui/core';
-import { SearchOutlined } from '@material-ui/icons';
-import React, { useState } from "react";
-import logo from '../logo_2.png'
-import icon from '../icon.png'
-import { Box } from '@material-ui/core';
-import { useNavigate } from 'react-router-dom';
-import { dummyData } from "../data";
-import {useLocation} from 'react-router-dom';
+import React, { useEffect, useState } from "react"
+import { useLocation } from 'react-router-dom';
 
-import Async from 'react-async';
+const UsingFetch = () => {
+  const [users, setUsers] = useState([])
+  let { state } = useLocation();
+  let query = state["searchTerm"];
+  console.log(query)
 
-const loadUsers = (query) =>
-  fetch("http://127.0.0.1:8000/search/?query=$".replace("$", query))
-    .then(res => (res.ok ? res : Promise.reject(res)))
-    .then(res => res.json());
+  const fetchData = () => {
+    fetch("http://127.0.0.1:8000/search/?query=$".replace("$", query))
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setUsers(data["0"])
+      })
+  }
 
-// Our component
-function ResultsPage() {
-  const { state } = useLocation();
-  var query = state["searchTerm"];
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  // TODO: WINI PLEASE MAKE THIS LOOK NICE!
   return (
-    <div className="container">
-      <Async promiseFn={loadUsers(query)}>
-        {({ data, err, isLoading }) => {
-          if (isLoading) return "Loading..."
-          if (err) return `Something went wrong: ${err.message}`
-
-          if (data)
-            console.log(data)
-            return (
-              <div>
-                <div>
-                  <h2>React Async - Random Users</h2>
-                </div>
-                {data["0"].map(user=> (
-                  <div key={user.title} className="row">
-                    <div className="col-md-12">
-                      <p>{user.link}</p>
-                      <p>{user.description}</p>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            )
-        }}
-      </Async>
+    <div>
+      {users.length > 0 && (
+        <ul>
+          {users.map(user => (
+            <li key={user.title}>
+              {user.title}
+              <br></br>
+              {user.link}
+              <br></br>
+              {user.description}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
-  );
+  )
 }
 
-export default ResultsPage;
-
-// DATA IS THIS!
-// #   "0": [
-// #     {
-//   #       "title": "69171296",
-//   #       "link": "https://en.wikipedia.org/?curid=69171296",
-//   #       "description": "69171296"
-//   #     },
-//   #     {
-//   #       "title": "69171296",
-//   #       "link": "https://en.wikipedia.org/?curid=69171296",
-//   #       "description": "69171296"
-//   #     }
-//   #   ]
-//   # }
+export default UsingFetch
